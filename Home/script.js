bfb28e4097527433a12913068fccbc85c4f5948d
Phanendra Sai Ram <125650236+heisenberg3376@users.noreply.github.com>
2023-08-30 16:00:42 +0530

    Delete Home directory

--- Home/script.js
+++
@@ -1,8 +0,0 @@
-var crsr = document.querySelector('.cursor')
-var  main = document.querySelector('*')
-
-main.addEventListener('mousemove',function(h){
-    console.log(h.x)
-    crsr.style.left = h.x + 'px'
-    crsr.style.top = h.y + 'px'
-})
